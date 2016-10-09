6c1e473b23d6f07b7edfc5a6a35d01660ecdc8bf
David Eugenio Perez Negron Rocha <daroperez_negron@esimez.mx>
2016-10-09 16:49:13 -0500

    Starting With cx_Oracle and oracle-db-query test

---
+++ ConConf.py
@@ -0,0 +1,10 @@
+'''Datos para conexion a Oracle'''
+ODbData={'username': 'root',
+        'password': 'somepasswd',
+        'address': 'somehost',
+        'database': 'somedb'}
+'''Datos para conexion a Hive'''
+HDbData={'user': 'root',
+        'password': 'somepasswd',
+        'host': 'somehost',
+        'port': 123}
